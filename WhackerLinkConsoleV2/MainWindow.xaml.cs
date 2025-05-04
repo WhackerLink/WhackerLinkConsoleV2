fa8fadc5e376d9d53b9d3f6718f6226f612f9963
firealarmss <ko4uyj@gmail.com>
2025-05-04 18:26:19 -0500

    add debug logging

--- WhackerLinkConsoleV2/MainWindow.xaml.cs
+++ WhackerLinkConsoleV2/MainWindow.xaml.cs
@@ -42,7 +42,6 @@ using fnecore.P25;
 using fnecore;
 using Microsoft.VisualBasic;
 using System.Text;
-using Nancy;
 using Constants = fnecore.Constants;
 using System.Security.Cryptography;
 using fnecore.P25.LC.TSBK;
@@ -497,6 +496,10 @@ namespace WhackerLinkConsoleV2
                                 }
                             };
 
+#if DEBUG
+                            Console.WriteLine($"WLINK AUDIO_DATA SrcId: {system.Rid}; DstId: {cpgChannel.Tgid}; Freq: {channel.VoiceChannel}");
+#endif
+
                             handler.SendMessage(voicePaket);
                         }
                     }
@@ -634,9 +637,8 @@ namespace WhackerLinkConsoleV2
                     };
 
                     byte[] tsbk = new byte[P25Defines.P25_TSBK_LENGTH_BYTES];
-                    byte[] payload = new byte[P25Defines.P25_TSBK_LENGTH_BYTES];
 
-                    callAlert.Encode(ref tsbk, ref payload, true, true);
+                    callAlert.Encode(ref tsbk, true, true);
 
                     handler.SendP25TSBK(callData, tsbk);
 
